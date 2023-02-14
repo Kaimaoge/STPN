f6a9fcb8db71b1d3493a58fff6bf9be982d84192
Yuankai Wu <Kaimaogege@gmail.com>
2023-02-14 14:27:39 +0800

    Add files via upload

---
+++ PeMS/PeMS_exp.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 15:24:24 2023
+
+@author: AA
+"""
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import numpy as np
+from PeMS_Process import ForecastDataset,ForecastTestDataset, de_normalized
+from torch.utils.data import DataLoader
+from stpn_pems import STPN
+import torch.optim as optim
+import torch
+import util
+import torch.nn as nn
+from PeMS_evaluate import evaluate
+import copy
+import pandas as pd
+
+
+if __name__ ==  '__main__':
+    dataset = '08'
+    
+    data_file = os.path.join('PeMS/PEMS' + dataset + '.npz')
+    print('data file:',data_file)
+    data = np.load(data_file,allow_pickle=True)
+    data = data['data'][:,:,0]
+    train_ratio = 6 / (6 + 2 + 2)
+    valid_ratio = 2 / (6 + 2 + 2)
+    test_ratio = 1 - train_ratio - valid_ratio
+    train_data = data[:int(train_ratio * len(data))]
+    valid_data = data[int(train_ratio * len(data)):int((train_ratio + valid_ratio) * len(data))]
+    test_data = data[int((train_ratio + valid_ratio) * len(data)):]
+    begin_time = int((train_ratio + valid_ratio) * len(data))
+    train_mean = np.mean(train_data, axis=0)
+    train_std = np.std(train_data, axis=0)
+    train_normalize_statistic = {"mean": train_mean.tolist(), "std": train_std.tolist()}
+    val_mean = np.mean(valid_data, axis=0)
+    val_std = np.std(valid_data, axis=0)
+    val_normalize_statistic = {"mean": val_mean.tolist(), "std": val_std.tolist()}
+    test_mean = np.mean(test_data, axis=0)
+    test_std = np.std(test_data, axis=0)
+    test_normalize_statistic = {"mean": test_mean.tolist(), "std": test_std.tolist()} # data_leakage
+    train_set = ForecastDataset(train_data, window_size=12, horizon=12,
+                            normalize_method='z_score', norm_statistic=train_normalize_statistic)
+    test_set = ForecastTestDataset(test_data, window_size=12, horizon=12, begin_time = begin_time,
+                                normalize_method='z_score', norm_statistic=test_normalize_statistic)
+    
+    train_loader = DataLoader(train_set, batch_size=32, drop_last=False, shuffle=True,
+                                        num_workers=1)
+    test_loader = DataLoader(test_set, batch_size=32, shuffle=False, num_workers=1)
+    
+    data_d = np.reshape(data, [data.shape[0]//288, 288, data.shape[1]])
+    daily_trend = np.mean(data_d, axis = 0)
+    daily_diff = daily_trend[1:, :] - daily_trend[:-1, :] 
+    cov = np.corrcoef(daily_trend.transpose())
+    cov[cov < 0.95] = 0
+    cov_d = np.corrcoef(daily_diff.transpose())
+    cov_d[cov_d < 0.6] = 0
+    con = pd.read_csv('PeMS/adj_PEMS' + dataset + '.csv', header=None)
+    con = np.array(con)
+    adj = [util.asym_adj(cov), util.asym_adj(cov_d), util.asym_adj(con)]
+    supports = [torch.tensor(i).to('cuda') for i in adj]
+    
+    h1 = 128
+    h2 = 64
+    h3 = 32
+    l = 2
+    model = STPN(l, 1, [h1, h2, h3], 1, 16, 0, support_len = 3)
+    model.to('cuda')
+    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+    forecast_loss = nn.L1Loss().cuda()
+    MAE_list = []
+    for epoch in range(1, 201):
+        model.train()
+        loss_total = 0
+        loss_total_F = 0
+        loss_total_M = 0
+        for i, (inputs, inputs_t, target, target_t) in enumerate(train_loader):
+            inputs = inputs.cuda()  # torch.Size([32, 12, 228])
+            target = target.cuda()
+            inputs_t = inputs_t.cuda()
+            target_t = target_t.cuda()
+            inputs = torch.unsqueeze(inputs, 1).permute(0, 1, 3, 2)
+            target = torch.unsqueeze(target, 1).permute(0, 1, 3, 2)
+            model.zero_grad()
+            output = model(inputs, inputs_t, supports, target_t)
+            loss = forecast_loss(output, target)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 3)
+            optimizer.step()
+        
+        forecast_set = []
+        target_set = []
+        model.eval()
+        with torch.no_grad():
+            for i, (inputs, inputs_t, target, target_t) in enumerate(test_loader):
+                inputs = inputs.cuda()  # torch.Size([32, 12, 228])
+                target = target.cuda()
+                inputs_t = inputs_t.cuda()
+                target_t = target_t.cuda()
+                inputs = torch.unsqueeze(inputs, 1).permute(0, 1, 3, 2)
+                target = torch.unsqueeze(target, 1).permute(0, 1, 3, 2)
+                output = model(inputs, inputs_t, supports, target_t)
+                forecast_set.append(output.detach().cpu().numpy())
+                target_set.append(target.detach().cpu().numpy())
+            result_save = np.concatenate(forecast_set, axis=0)
+            target_save = np.concatenate(target_set, axis=0)
+        
+        result = de_normalized(result_save[:, 0, :, :].transpose(0, 2, 1), 'z_score', test_normalize_statistic)
+        target = de_normalized(target_save[:, 0, :, :].transpose(0, 2, 1), 'z_score', test_normalize_statistic)
+        MAPE, MAE, RMSE = evaluate(result, target)
+        log = 'On average, Test MAE: {:.4f}, Test MAPE: {:.4f}, Test RMSE: {:.4f}'
+        print(log.format(MAE,MAPE,RMSE))
+        MAE_list.append(MAE)
+        if MAE == min(MAE_list):
+            best_model = copy.deepcopy(model.state_dict())
+        
+        model.load_state_dict(best_model)
+        torch.save(model, "spdn_pems08" + str(l) + str(h1) + str(h2) +".pth")
---
+++ PeMS/stpn_pems.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 16:07:14 2023
+
+@author: AA
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class nconv(nn.Module):
+    def __init__(self):
+        super(nconv,self).__init__()
+
+    def forward(self,x, A):
+        x = torch.einsum('ncvl,vw->ncwl',(x,A))
+        return x.contiguous()
+
+class linear(nn.Module):
+    def __init__(self,c_in,c_out):
+        super(linear,self).__init__()
+        self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 1), padding=(0,0), stride=(1,1), bias=True)
+
+    def forward(self,x):
+        return self.mlp(x)
+
+class gcn(nn.Module):
+    def __init__(self,c_in,support_len=3,order=2):
+        super(gcn,self).__init__()
+        self.nconv = nconv()
+        self.order = order
+
+    def forward(self,x,support):
+        out = [x]
+        for a in support:
+            x1 = self.nconv(x,a)
+            out.append(x1)
+            for k in range(2, self.order + 1):
+                x2 = self.nconv(x1,a)
+                out.append(x2)
+                x1 = x2
+
+        h = torch.cat(out,dim=1)
+        return h
+    
+class learnEmbedding(nn.Module):
+    def __init__(self, d_model):
+        super(learnEmbedding, self).__init__()
+        self.factor = nn.parameter.Parameter(torch.randn(1,),requires_grad=True).to('cuda')
+        self.d_model = d_model
+    
+    def forward(self, x):
+        div = torch.arange(0, self.d_model, 2).to('cuda')
+        div_term = torch.exp(div * self.factor)
+        if len(x.shape) == 2:
+            v1 = torch.sin(torch.einsum('bt, f->btf', x, div_term))
+            v2 = torch.cos(torch.einsum('bt, f->btf', x, div_term))
+        else:
+            v1 = torch.sin(torch.einsum('bvz, f->bvzf', x, div_term))
+            v2 = torch.cos(torch.einsum('bvz, f->bvzf', x, div_term))
+        return torch.cat([v1, v2], -1)
+    
+class ATT(nn.Module):
+    def __init__(self,c_in, d = 16, device = 'cuda'):
+        super(ATT,self).__init__()
+        self.d = d
+        self.qm = nn.Linear(in_features = c_in, out_features = d, bias = False)
+        self.km = nn.Linear(in_features = c_in, out_features = d, bias = False)
+        self.device = device
+    
+    def forward(self,x, y):
+        if len(x.shape) == 3:
+            query = self.qm(y)
+            key = self.km(x)
+            attention = torch.einsum('btf,bpf->btp', query, key)
+            attention /= (self.d ** 0.5)
+            attention = F.softmax(attention, dim=-1)
+        else:
+            query = self.qm(y)
+            key = self.km(x)
+            attention = torch.einsum('bvzf,buzf->bvu', query, key)
+            attention /= (self.d ** 0.5)
+            attention = F.softmax(attention, dim=2)
+        return attention
+    
+
+class STMH_GCNN_layer(nn.Module):
+    """
+    Shape:
+        - Input[0]: Input graph sequence in :math:`(N, in_channels, V, T_in)` format
+        - Input[1]: Input random walk matrix in a list :math:`(V, V)` format
+        - INput[2]: Input time label :math:`(N, T)`
+        - Output[0]: Output graph sequence in :math:`(N, out_channels, V, T_out)` format
+        where
+            :math:`N` is a batch size,
+            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
+            :math:`T_{in}/T_{out}` is a length of input/output sequence,
+            :math:`V` is the number of graph nodes. 
+            :in_channels= dimension of coordinates
+            : out_channels=dimension of coordinates
+            +
+    """
+    def __init__(self, in_channels, out_channels, emb_size, dropout, time_d = 16, heads = 4, support_len = 1, order = 2, final_layer = False):
+        super(STMH_GCNN_layer,self).__init__()   
+        self.gcn = gcn(in_channels,support_len=support_len,order=order)
+        gc_in = (order*support_len+1)*in_channels
+        self.out = linear(gc_in, out_channels)
+        self.temb = nn.ModuleList()
+        self.tgraph = nn.ModuleList()
+        for i in range(heads):
+            self.temb.append(learnEmbedding(emb_size))
+            self.tgraph.append(ATT(emb_size, time_d))
+        if in_channels != out_channels: 
+            self.residual=nn.Sequential(linear(in_channels, out_channels),
+                nn.BatchNorm2d(out_channels),
+            )                   
+        else:
+            self.residual=nn.Identity()       
+        self.prelu = nn.PReLU()
+        self.final_layer = final_layer
+        self.dropout = dropout
+        self.heads = heads
+        
+    def forward(self, x, t_in, supports, t_out = None):
+        t_att = []
+        for i in range(self.heads):
+            k_emb = self.temb[i](t_in)
+            if t_out == None:
+                q_emb = k_emb         
+            else:
+                q_emb = self.temb[i](t_out)        
+            t_att.append(self.tgraph[i](k_emb, q_emb))
+        res=self.residual(x)
+        xt = torch.einsum('ncvt,npt->ncvp', (x, t_att[0]))
+        for i in range(self.heads - 1):
+            xt += torch.einsum('ncvt,npt->ncvp', (x, t_att[i+1])) 
+        x = self.gcn(xt, supports)
+        x = self.out(x)
+        if not self.final_layer:
+            x = x+res
+            x = self.prelu(x)
+        x = F.dropout(x, self.dropout, training=self.training)
+        
+        return x
+    
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+            
+
+class STPN(nn.Module):
+    """
+    Shape:
+        - Input[0]: Input graph sequence in :math:`(N, in_channels, V, T_in)` format
+        - Input[1]: Input time label :math:`(N, T_in)` format   
+        - Input[2]: Input random walk matrix in a list :math:`(V, V)` format
+        - Input[3]: Output time label :math:`(N, T_out)`
+        - Input[4]: Input covariate sequence in :math:`(N, V, T_out)`
+        - Output[0]: Output graph sequence in :math:`(N, out_channels, V, T_out)` format
+        where
+            :math:`N` is a batch size,
+            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
+            :math:`T_{in}/T_{out}` is a length of input/output sequence,
+            :math:`V` is the number of graph nodes. 
+            :in_channels= dimension of coordinates
+            :out_channels= dimension of coordinates
+            +
+    """
+    def __init__(self, h_layers, in_channels, hidden_channels, out_channels, emb_size, dropout,  time_d = 4, heads = 4, support_len = 3, order = 2, use_se = True):
+        super(STPN,self).__init__()
+        """
+        Params:
+            - h_layers: number of hidden STS GCNs
+            - in_channels: num = 2 for delay prediction
+            - hidden_channels: a list of dimensions for hidden features
+            - out_channels: num = 2 for delay prediction
+            - emb_size: embedding size for self attention model
+            - dropout: dropout rate
+            - wemb_size: covariate embedding size
+            - time_d: d for self attention model
+            - heads: number of attention head
+            - supports_len: number of spatial adjacency matrix
+            - order: order of diffusion convolution
+            - num_weather: number of weather condition
+            - use_se: whether use SE block
+            - use_cov: whether use weather information
+        """
+      
+        self.h_layers = h_layers
+        self.convs = nn.ModuleList()
+        self.se = nn.ModuleList()
+        self.use_se = use_se
+        self.convs.append(STMH_GCNN_layer(in_channels, hidden_channels[0], emb_size, dropout, time_d, heads, support_len, order, False))
+        for i in range(h_layers):
+            if self.use_se:
+                self.se.append(SELayer(hidden_channels[i]))
+            self.convs.append(STMH_GCNN_layer(hidden_channels[i], hidden_channels[i+1], emb_size, dropout, time_d, heads, support_len, order, False))  
+        self.final_conv = STMH_GCNN_layer(hidden_channels[h_layers] , out_channels, emb_size, dropout, time_d, heads, support_len, order, True)
+         
+    def forward(self, x, t_in, supports, t_out):
+        for i in range(self.h_layers + 1):
+            x = self.convs[i](x, t_in, supports)
+            if i < self.h_layers and self.use_se:
+                x = self.se[i](x)
+        out = self.final_conv(x, t_in, supports, t_out)     
+        return out
